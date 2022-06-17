807536d48b0cacaf6c1c2a59858c75dab17c54f9
SwartzJonah <swartzjonah@gmail.com>
2022-06-16 22:19:43 -0400

    added javascript checking if passwords matched

---
+++ javascript.js
@@ -0,0 +1,18 @@
+
+const form  = document.getElementById('signupForm');
+let password = document.getElementById("password").value;
+let confirmpassword = document.getElementById("confirmpassword").value;
+
+
+
+
+form.addEventListener('submit',function (event) {
+    password = document.getElementById("password").value;
+    confirmpassword = document.getElementById("confirmpassword").value;
+    if (password != confirmpassword) {
+        console.log(password);
+        console.log(confirmpassword);
+        alert("Password and Confirm Password must match")
+        event.preventDefault()
+    }
+});
